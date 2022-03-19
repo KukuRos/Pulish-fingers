c5e9d12ab693d524b2d211b2350f71557ed92343
KukuRos <micaela.m.rosa@gmail.com>
2022-03-19 08:05:27 -0300

    function

--- figures.js
+++ figures.js
@@ -1,54 +1,42 @@
-//Código del cuadrado
-
-console.group("Cuadrado")
-
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
-
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
-
-console.groupEnd()
-
-//Código del triángulo
-
-console.group("Triángulo")
-
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
+///Código del cuadrado
 
 
-console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cm. Y " + ladoTriangulo2 + "cm");
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perímetro del triangulo es: " + perimetroTriangulo + "cm");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
 
-const areaTriangulo = (baseTriangulo * alturaTriangulo)/2;
-console.log("El área del triangulo es: " + areaTriangulo + "cm2");
+function areaCuadrado(lado) {
+    return lado * lado;
+}
 
-console.groupEnd()
 
-//Código del rectángulo
+
+///Código del triángulo
+
+
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+    return (base * altura)/2;
+}
+
 
 
 //Código del círculo
 
-console.group("Círculo")
 
-const radioCirculo = 4;
-const diametroCirculo = radioCirculo*2;
+
 const PI = Math.PI;
 
+function perimetroCirculo(diametro) {
+    return diametro*PI;
+}
 
-const perimetroCirculo = diametroCirculo * PI;
-console.log("El perímetro del círculo es: " + perimetroTriangulo + "cm");
+function areaCirculo(radio) {
+    return (radio * radio)*PI;
+}
 
-const areaCirculo = (radioCirculo * radioCirculo)*PI;
-console.log("El área del círculo es: " + areaCirculo + "cm2");
-
-console.groupEnd()
